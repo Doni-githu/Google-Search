9624c92ce28513367354fe3c5281000eafc557d4
Doniyor-githu <ddonierov68@gmail.com>
2023-02-17 18:59:47 +0500

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import pprint
-
+from deep_translator import GoogleTranslator
 import requests
 
 from aiogram import Dispatcher, Bot, executor, types
@@ -25,13 +25,14 @@ async def post(message: types.Message):
         "X-RapidAPI-Key": "309762a3f1mshb8816378be4c2bcp1e702fjsn568bcbe3baf1",
         "X-RapidAPI-Host": "google-web-search1.p.rapidapi.com"
     }
-
     response = requests.request("GET", url, headers=headers, params=querystring).text
     data = json.loads(response)
     pprint.pprint(data['knowledge_panel']['description']['text'])
     knowledge = data['knowledge_panel']
+    pprint.pprint(knowledge)
     description = knowledge['description']
     text = description['text']
+    GoogleTranslator()
     await message.answer(text)
 
 
